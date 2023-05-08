cb207bd71683cabb2c0eceba8feb8a4857338b45
Horrid Entity <etienne.olieu@gmail.com>
2023-05-08 10:10:47 +0200

    first commit, basic server and client

---
+++ server/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+const app = express();
+
+
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, (error)=>{
+    if(error){
+        console.log(error);
+    }
+    console.log("Server is running on PORT:", PORT);
+});
