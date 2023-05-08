d5058ae274d0a8278207b16f6ccde01204a570d7
Horrid Entity <etienne.olieu@gmail.com>
2023-05-08 11:38:57 +0200

    created basis

---
+++ client/src/pages/home/Home.svelte
@@ -0,0 +1,15 @@
+<script>
+
+
+</script>
+
+<h1>
+    GRE&#91;en te&#93;AM
+</h1>
+<h3>
+    The place to manage all tasks associated with maintenance of the common areas
+</h3>
+<p>
+Here you will find information about all the tasks that need doing, a list of available materiel as well as a map of the area.
+</p>
+
